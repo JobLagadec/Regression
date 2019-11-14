757e6137a1033ed87889aaa0d2024b29c6349272
Alexis GUERMONT <a17guerm@pc-df-893.priv.enst-bretagne.fr>
2019-11-14 16:30:11 +0100

    functions to clean and normalize csv data added

---
+++ main.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+from math import *
+import pandas as pda
+from sklearn.preprocessing import scale
+
+housing_file = "data_regression/HousingData.csv"
+
+"""auteur: Alexis """
+
+def load_csv(file_name):
+    csv_data = pda.read_csv(file_name)
+    data = csv_data.values
+    return data
+
+housing_data = load_csv(housing_file)
+
+def mean(column):
+    l = len(column)
+    mean = 0
+    for k in range(l):
+        if(np.isnan(column[k]) == False):
+            mean += column[k]
+    return mean/l
+
+def clean_csv_data(data):
+    n, m = data.shape
+    res = data
+    means = np.zeros(m)    
+    for k in range(m):
+        means[k] = mean(data[:,k])
+    #For each individuals and for each feature, if the value of the feature is unknown, we replace it by the mean
+    for i in range(n):
+        for j in range(m):
+            if (np.isnan(data[i][j])):
+                res[i][j] = means[j]
+    return res
+    
+def normalize_data(data):
+    res = data
+    n, m = data.shape
+    maxes = np.zeros(m)
+    for k in range(m):
+        maxes[k] = max(data[:,k])
+        res[:,k] = res[:,k]/maxes[k]
+    return res
+
+clean_data = clean_csv_data(housing_data)
+normalized_data = normalize_data(clean_data)
+
+"""auteur: Alexis """
