75056367a3d51e31a557be5cf17627c5c0c5c656
Alexis GUERMONT <a17guerm@pc-df-896.priv.enst-bretagne.fr>
2019-11-21 18:51:27 +0100

    kernel functions added and graphical results store

--- main.py
+++ main.py
@@ -62,10 +62,10 @@ auteur : Tom Dauve
 def load_data_data(filepath):
     df = pda.read_table(filepath)
     data = df.values
-    output  = []
-    for i in range(len(data)):
-        output.append(data[i][1:len(data[i])-1])
-    y = [output[i][-1] for i in range(len(output))]
+    output  = data[:,:9]
+    #for i in range(len(data)):
+        #output.append(data[i][1:len(data[i])-1])
+    y = data[:,9]
     return np.array(output), np.array(y)
 
 """
@@ -178,9 +178,76 @@ def Covariance_Matrix(x_data, y_data):
     y_data = np.reshape(y_data, (len(y_data),1)) # necessaire pour concatenation  
     data = np.concatenate((x_data,y_data),axis = 1)
     return np.cov(np.transpose(data))
-    
 
+"""auteur: Alexis """       
+#Function to approximate a gaussian (evaluated on x) that fits the points in data
+def kde(x, data, h=0):
+    estimated_mean = 0
+    estimated_variance = 0
+    n = len(data)
+    for k in range(n):
+        estimated_mean += data[k]
+    estimated_mean = estimated_mean/n
+    for i in range(n):
+        estimated_variance += (data[i] - estimated_mean/n)**2
+    estimated_variance = estimated_variance/n
+    if (h == 0):
+        h = 1.06*sqrt(estimated_variance)*n**(-1/5)
+    L = len(x)
+    y = np.zeros(L)
+    for j in range(L):
+        su = 0
+        for k in range(n):
+            su += exp(-(((x[j] - data[k])/h - estimated_mean)**2)/(2*estimated_variance))/(sqrt(2*np.pi*estimated_variance))
+        y[j] = su/(n*h)
+    return y
 
+"""auteur: Alexis """   
+#gaussian kernel density estimator evaluated on x, that fits the relationship between data1 and data2 
+def kde_2D(x, data1, data2, h=0):
+    estimated_mean = 0
+    estimated_variance = 0
+    n = len(data1)
+    for k in range(n):
+        estimated_mean += data1[k]
+    estimated_mean = estimated_mean/n
+    for i in range(n):
+        estimated_variance += (data1[i] - estimated_mean/n)**2
+    estimated_variance = estimated_variance/n
+    if (h == 0):
+        h = 1.06*sqrt(estimated_variance)*n**(-1/5)
+    L = len(x)
+    y = np.zeros(L)
+    for j in range(L):
+        sum1 = 0
+        sum2 = 0
+        for k in range(n):
+            sum1 += data2[k]*np.exp(-(((x[j] - data1[k])/h - estimated_mean)**2)/(2*estimated_variance))/(sqrt(2*np.pi*estimated_variance))
+            sum2 += np.exp(-(((x[j] - data1[k])/h - estimated_mean)**2)/(2*estimated_variance))/(sqrt(2*np.pi*estimated_variance))
+        
+        y[j] = sum1/sum2
+    return y
 
+"""auteur: Alexis """   
+#mean square error function
+def mse(target, estimation):
+    n = len(target)
+    res = 0
+    for k in range(n):
+        res += (target[k] - estimation[k])**2
+    return res/n
 
+x, y = get_trainable_data(prostate_file)
+l = np.arange(-0.5, 6, 0.1)
+y_hat = kde(l, y, h=0.1)
 
+plt.plot(l, y_hat)
+plt.show()
+plt.scatter(x[:,0], y)
+plt.show()
+
+y_all = kde_2D(x[:,0], x[:,0], y, h=0.05)
+print("mean squared error on whole dataset = " + str(mse(y, y_all)))
+plt.scatter(x[:,0], y)
+plt.scatter(x[:,0], y_all, c='r')
+plt.show()
